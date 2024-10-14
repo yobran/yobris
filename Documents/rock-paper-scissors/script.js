1442c6b6dac49f8e8d06209c630c71835b1c2c55
yobran <kipkiruibrian925@gmail.com>
2024-10-14 16:48:52 +0300

    Initial setup: Create HTML file and link JavaScript

---
+++ Documents/rock-paper-scissors/script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    const randomNum = Math.random();
+    if (randomNum < 0.33) {
+        return "rock";
+    } else if (randomNum < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
